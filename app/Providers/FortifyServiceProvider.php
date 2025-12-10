8cab60c7d19ee1e86410426d1ad4d2d1b5661e1c
Juan M. Cortéz <juanm.cortez@gmail.com>
2025-12-10 02:40:58 -0300

    🚧 New auth views + User login via username or email.

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\Users\User;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Laravel\Fortify\Fortify;
+use Illuminate\Support\Facades\Hash;
 use App\Actions\Fortify\CreateNewUser;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Cache\RateLimiting\Limit;
@@ -29,8 +31,47 @@ class FortifyServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // Registering Views
-        Fortify::loginView(function () {
+        Fortify::loginView(static function () {
             return view('auth.login');
+        });
+
+        Fortify::registerView(static function () {
+            return view('auth.register');
+        });
+
+        Fortify::requestPasswordResetLinkView(static function () {
+            return view('auth.forgot-password');
+        });
+
+        Fortify::resetPasswordView(static function (Request $request) {
+            return view('auth.reset-password', ['request' => $request]);
+        });
+
+        Fortify::verifyEmailView(static function () {
+            return view('auth.verify-email');
+        });
+
+        // Override authenticate method
+        Fortify::authenticateUsing(static function (Request $request) {
+            // Pick only necessary data
+            $credentials = $request->only('username', 'password');
+            $username = Str::lower(Str::trim($credentials['username']));
+            // Find user by username
+            $user = User::whereUsername($username)->first();
+            // If not found by username, try to find by email through demographics
+            if (!$user) {
+                $user = User::whereHas('demographics.emails', static function ($query) use ($username) {
+                    $query->where('email', $username)
+                        ->primary()
+                        ->verified();
+                })->first();
+            }
+            // Verify the password
+            if ($user && Hash::check($credentials['password'], $user->password)) {
+                return $user;
+            }
+            // Everything failed
+            return null;
         });
 
         // New user registration conditions
@@ -40,12 +81,12 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
         // Fortify::redirectUserForTwoFactorAuthenticationUsing(RedirectIfTwoFactorAuthenticatable::class);
 
-        RateLimiter::for('login', function (Request $request) {
+        RateLimiter::for('login', static function (Request $request) {
             $throttleKey = Str::transliterate(Str::lower($request->input(Fortify::username())).'|'.$request->ip());
             return Limit::perMinute(5)->by($throttleKey);
         });
 
-        RateLimiter::for('two-factor', function (Request $request) {
+        RateLimiter::for('two-factor', static function (Request $request) {
             return Limit::perMinute(5)->by($request->session()->get('login.id'));
         });
     }
